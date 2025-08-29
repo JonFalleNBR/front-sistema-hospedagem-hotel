00168066e84b60a75d8b6ca0a75c35c56c5c8760
Jonathan <jonathan.leoesdejuda@gmail.com>
2025-08-28 23:21:54 -0300

    feat-Comeco-Estruturação-Cadastro-Hospede

---
+++ src/app/models/hospede.model.ts
@@ -0,0 +1,21 @@
+export interface Hospede {
+  id: number;
+  nome: string;
+  cpf: string;
+  email?: string | null;
+  telefone?: string | null;
+  dataCadastro: string; 
+
+  //reservas: Reserva[];
+
+
+}
+
+export interface HospedeCreatDto {
+
+nome: String; 
+cpf: string ; 
+email?: string; 
+telefone?: string; 
+
+}
---
+++ src/app/services/hospede.service.ts
@@ -0,0 +1,1 @@
+//TODO implementar serviço de hóspedes 
